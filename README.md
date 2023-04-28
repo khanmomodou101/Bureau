8a33c506ef83949e91de5aacaa78b1c0ab2a5efe
khan <khan@momodou-Latitude-3189>
2023-04-28 18:52:58 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Bureau
+
+Money Dashboard App
+
+#### License
+
+MIT
---
+++ bureau/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Bureau",
+			"type": "module",
+			"label": _("Bureau")
+		}
+	]
